4c4baa1982f0c420dacc6a6dcb2b80a070fcaf43
Anara Kulmambetova <anara.akimkanova.kk@gmail.com>
2021-10-15 00:47:23 +0400

    added penguin

--- script.js
+++ script.js
@@ -50,41 +50,41 @@
 // }
 
 //e) Comparators and Equality ------- && ------ || ------ect
-// prompt("What is your name?");
-// prompt("What is their name?");
+prompt("What is your name?");
+prompt("What is your favorite mountain in Kyrgyzstan?");
 
-// let loveScore = Math.random() * 100;
-// loveScore = Math.floor(loveScore) + 1;
+let loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore) + 1;
 
-// if (loveScore > 55){
-//     alert("Your love score is " + loveScore + "% chyragym! Masha Allah, may Allah bless You!");
-//     console.log("Your love score is " + loveScore + "% chyragym!Masha Allah, may Allah bless You!");
-// }
-// if (loveScore > 15 && loveScore <= 55) {
-//     alert("Your love score is " + loveScore + "% chyragym! This is the harmony life!");
-//     console.log("Your love score is " + loveScore + "% chyragym! This is the harmony life!");
-// }
-// if (loveScore <= 15) {
-//     alert("Your love score is " + loveScore + "% chyragym! This is JS math random calculating only.");
-//     console.log("Your love score is " + loveScore + "% chyragym! This is JS math random calculating only.");
-// }
+if (loveScore > 55){
+    alert("Your love score is " + loveScore + "% chyragym! Masha Allah, you love so much your Kyrgyzstan!");
+    console.log("Your love score is " + loveScore + "% chyragym!Masha Allah, you love so much your Kyrgyzstan!");
+}
+if (loveScore > 15 && loveScore <= 55) {
+    alert("Your love score is " + loveScore + "% chyragym! Ala Too love you and you love Ala too as well!");
+    console.log("Your love score is " + loveScore + "% chyragym! Ala Too love you and you love Ala too as well!");
+}
+if (loveScore <= 15) {
+    alert("Your love score is " + loveScore + "% chyragym! This is JS math random calculating only.");
+    console.log("Your love score is " + loveScore + "% chyragym! This is JS math random calculating only.");
+}
 
 //j)
 // BMI Calculator ------ Advanced (IF/ELSE) ----------
 
-function bmiCalculator(weight, height) {
-    var bmi = weight / Math.pow(height, 2);
-    var interpretation = "";
-    if (bmi < 18.5) {
-        interpretation = "Your BMI is " + bmi + ", so you are underweight.";
-    }
-    if (bmi >= 18.5 && bmi <= 24.9) {
-        interpretation = "Your BMI is " + bmi + ", so you have a normal weight.";
-    }
-    if (bmi > 24.9) {
-        interpretation = "Your BMI is " + bmi + ", so you are overweight.";
-    }
-    return interpretation;
-}
+// function bmiCalculator(weight, height) {
+//     var bmi = weight / Math.pow(height, 2);
+//     var interpretation = "";
+//     if (bmi < 18.5) {
+//         interpretation = "Your BMI is " + bmi + ", so you are underweight.";
+//     }
+//     if (bmi >= 18.5 && bmi <= 24.9) {
+//         interpretation = "Your BMI is " + bmi + ", so you have a normal weight.";
+//     }
+//     if (bmi > 24.9) {
+//         interpretation = "Your BMI is " + bmi + ", so you are overweight.";
+//     }
+//     return interpretation;
+// }
 
-bmiCalculator(64, 1.75);
+// bmiCalculator(64, 1.75);
